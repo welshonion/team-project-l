14d5ef56c23923a896c1493a40d8c2316e4723f1
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2022-12-27 17:44:45 +0900

    feat:画像の追加:地図ちゃんが動きます

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './figs/tizutyan.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          なんか回転する
         </p>
         <a
           className="App-link"
