5fdf17e21de0651090354d3bed5703d1868bbb7b
YwelshonionY <welshonion.ta.84@gmail.com>
2022-12-28 17:04:22 +0900

    feature: 地域選択に対応
    地域の選択画面を追加
    その他データを利用しやすいようコード追加

--- index.py
+++ index.py
@@ -11,116 +11,74 @@ from datetime import timedelta
 import requests
 import random
 
+import rakutenapi
+
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-get_area_class_url = 'https://app.rakuten.co.jp/services/api/Travel/GetAreaClass/20131024'
-simple_hotel_search_url = 'https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426'
+AREA_NUM = 3
 
-application_id = os.environ['RAKUTEN_APP_ID']
-
+# ホーム画面
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/result')
-def authorize():
+# 地域選択
+@app.route('/select')
+def select():
 
-    print("*****\ndecide hotel")
+    # 日本全体の地域情報をjsonで取得
+    area_data_json = rakutenapi.get_area_data_json()
 
-    #####場所の抽選#####
+    # 指定数だけ地域情報を取得
+    area_info_list = []
+    area_info_dict_list = []
+    for area_idx in range(AREA_NUM):
+        
+        # 地域を抽選し情報を取得
+        area_info = rakutenapi.AreaInfo()
+        area_info.load_from_api(area_data_json)
 
-    # パラメータ設定
-    param = {
-        "applicationId" : application_id,
-        "format" : "json"
-    }
+        # リストに格納
+        area_info_list.append(area_info)
+        area_info_dict_list.append(area_info.get_dict())
 
-    # APIを実行して結果を取得する
-    result = requests.get(get_area_class_url, param)
-    json_result = result.json()
-    #print(json_result)
+        # セッションに保存
+        session[f'area_info_json_{area_idx}'] = area_info.get_json()
 
+    return render_template(
+        'select.html',
+        area_num = AREA_NUM,
+        area_info_dict_list = area_info_dict_list
+        )
 
-    # 都道府県の抽選
-    middle_classes = json_result['areaClasses']['largeClasses'][0]['largeClass'][1]['middleClasses']
-    #print(len(middle_classes))
-    middle_idx = random.randint(0,len(middle_classes)-1)
-    middle_class_code = middle_classes[middle_idx]['middleClass'][0]['middleClassCode']
-    middle_class_name = middle_classes[middle_idx]['middleClass'][0]['middleClassName']
-    print(middle_class_name)
+@app.route('/result/<int:selected_area>', methods=['GET'])
+def result(selected_area):
 
-    # 地域の抽選
-    small_classes = middle_classes[middle_idx]['middleClass'][1]['smallClasses']
-    #print(len(small_classes))
-    small_idx = random.randint(0,len(small_classes)-1)
-    small_class_code = small_classes[small_idx]['smallClass'][0]['smallClassCode'] 
-    small_class_name = small_classes[small_idx]['smallClass'][0]['smallClassName'] 
-    print(small_class_name)
+    # 選択された地域の情報をセッションから取得
+    area_info_list = []
+    for area_idx in range(AREA_NUM):
+        area_info = rakutenapi.AreaInfo()
+        area_info.load_from_session(session[f'area_info_json_{area_idx}'])
+        area_info_list.append(area_info)
+    area_info = area_info_list[selected_area]
+    area_info_dict = area_info.get_dict()
 
-    detail_existed = len(small_classes[small_idx]['smallClass'])==2
+    # 地域全体のホテル情報をjsonで取得
+    hotel_data_json = rakutenapi.get_hotel_data_json(area_info=area_info)
 
-    # 詳細地域の抽選
-    if detail_existed:
-        detail_classes = small_classes[small_idx]['smallClass'][1]['detailClasses']
-        #print(len(detail_classes))
-        detail_idx = random.randint(0,len(detail_classes)-1)
-        detail_class_code = detail_classes[detail_idx]['detailClass']['detailClassCode'] 
-        detail_class_name = detail_classes[detail_idx]['detailClass']['detailClassName'] 
-        print(detail_class_name)
-    else:
-        detail_class_code = 0
-        detail_class_name = ""
+    # ホテルを抽選し情報を取得
+    hotel_info = rakutenapi.HotelInfo()
+    hotel_info.load_from_api(hotel_data_json)
 
-
-    #####旅館の抽選#####
-
-    if detail_existed:
-        # パラメータ設定
-        param = {
-            "applicationId" : application_id,
-            "format" : "json",
-            "largeClassCode" : "japan",
-            "middleClassCode" : middle_class_code,
-            "smallClassCode" : small_class_code,
-            "detailClassCode" : detail_class_code
-        }
-    else:
-        # パラメータ設定
-        param = {
-            "applicationId" : application_id,
-            "format" : "json",
-            "largeClassCode" : "japan",
-            "middleClassCode" : middle_class_code,
-            "smallClassCode" : small_class_code,
-        }
-
-    # APIを実行して結果を取得する
-    result = requests.get(simple_hotel_search_url, param)
-    json_result = result.json()
-    #print(json_result)
-
-    # 宿の抽選
-    hotels = json_result['hotels']
-    #print(len(hotels))
-    hotel_idx = random.randint(0,len(hotels)-1)
-    hotel = hotels[hotel_idx]['hotel']
-    hotel_name = hotel[0]['hotelBasicInfo']['hotelName']
-    hotel_rating_ave = hotel[0]['hotelBasicInfo']['reviewAverage']
-    hotel_url = hotel[0]['hotelBasicInfo']['hotelInformationUrl']
-    print(hotel_name)
-
+    # 辞書に変換
+    hotel_info_dict = hotel_info.get_dict()
 
     return render_template(
         'result.html', 
-        middle = middle_class_name,
-        small = small_class_name,
-        detail = detail_class_name,
-        detail_existed = detail_existed,
-        hotel_name = hotel_name,
-        hotel_rating_ave = hotel_rating_ave,
-        hotel_url = hotel_url
+        area_info_dict = area_info_dict,
+        hotel_info_dict = hotel_info_dict
         )
 
 if __name__ == '__main__':
