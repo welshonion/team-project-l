7ddac81925ce1a10f9d18746aa392b63c83a0220
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2022-12-30 03:44:05 +0900

    feat: トップページのハリボテ作成[WIP]

--- frontend/src/pages/topPage.js
+++ frontend/src/pages/topPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './style.css';
+import logo from '../figs/tizutyan.svg'
+import { Button} from '@material-ui/core';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 class topPage extends React.Component {   //topPageクラスにReact.Componentを継承する
  
     render() {                          //画面表示の為のrenderメソッドを定義する
         return (
-            <div>
-                HellotopPage!!!
+            <div className="App">
+                <h1 className="title">Iikanji no Title</h1>
+                <img src={logo} className="App-logo" alt="logo" />
+                <Button color="secondary" variant="contained" size="large" endIcon={<AddLocationAltIcon />}>
+                    旅する!
+                </Button>
+
             </div>
         );
     }
