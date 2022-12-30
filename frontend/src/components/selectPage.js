176feb80e1c51047c9c08451b1aa326ee480c395
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2022-12-30 17:58:19 +0900

    feat: selectPageの編集

--- frontend/src/components/selectPage.js
+++ frontend/src/components/selectPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './style.css';
 
 class selectPage extends React.Component {   //selectPageクラスにReact.Componentを継承する
  
     render() {                          //画面表示の為のrenderメソッドを定義する
         return (
             <div>
-                HelloselectPage!!!
+                <h2 className='h2'>・どこにする?</h2>
             </div>
         );
     }
