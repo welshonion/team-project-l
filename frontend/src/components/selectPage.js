ef3f0ee2870bf5ce1e2d51f8dd50b3945dc26fe7
fujiihisanori <105897778+fujiihisanori@users.noreply.github.com>
2023-01-05 19:36:19 +0900

    追加：フロントとバックの接続

--- frontend/src/components/selectPage.js
+++ frontend/src/components/selectPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{useState,useEffect,useMemo} from 'react';
+import Axios from 'axios';
 import './style.css';
 import aichi from '../figs/prefs/aichi.png'
 import akita from '../figs/prefs/akita.png'
@@ -19,75 +20,94 @@ const handleClick_select2 = () => {
     window.location.href = "/";
   };
 
-class selectPage extends React.Component {   //selectPageクラスにReact.Componentを継承する
- 
-    render() {                          //画面表示の為のrenderメソッドを定義する
-        return (
-            <div>
-                <div className='Main'>
-                    <h2 className='title'>・どこにする?</h2>
-                </div>
+const SelectPage = () => {
+    //const [areaNum, setAreaNum] = useState(0);
+    const [areaInfoDictList, setAreaInfoDictList] = useState([]);
 
-                
-                <div className='select'>
-                        <Button color="default" variant="contained" size='large' onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
-                            <table className="place">
-                                <tr>
-                                    <td><img src={aichi} width="200" alt="logo" /></td>
-                                </tr>
-                                <tr>
-                                    <tb className="table_font">愛知県  <br/>
-                                        ○○市・××市・△△市
-                                    </tb>
-                                </tr>
-                                
-                            </table>
+    useEffect(() =>{
+        try{
+            Axios.get("http://127.0.0.1:5000/select")
+                .then((res) => {
+                    //setAreaNum(res.data.area_num);
+                    setAreaInfoDictList(res.data.area_info_dict_list);
+                })
+        }catch(error){
+            console.error(error);
+        }   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    } , []);
+
+    const handleSubmit = (index) => {
+        try{
+            Axios.get(`http://127.0.0.1:5000/result`);
+        }catch(error){
+            console.error(error);
+        }   
+    }
+
+    return (
+        <div>
+            <div className='Main'>
+            <h2 className='title'>・どこにする?</h2>
+            </div>
+            {console.log(areaInfoDictList)}
+            <div className='select'>
+                    <Button color="default" variant="contained" size='large' onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
+                        <table className="place">
+                            <tr>
+                                <td><img src={aichi} width="200" alt="logo" /></td>
+                            </tr>
+                            <tr>
+                                <tb className="table_font">{areaInfoDictList.length ? areaInfoDictList[0]["middle_class_name"] : ""}<br/>
+                                {areaInfoDictList.length ? areaInfoDictList[0]["small_class_name"] : ""}
+                                </tb>
+                            </tr>
                             
-                        </Button>
+                        </table>
                         
+                    </Button>
 
-                        <Button color='default' variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' style={{ margin: "5%"}}>
-                            <table className="place">
-                                <tr>
-                                    <td><img src={akita}  height="200" alt="logo" /></td>
-                                </tr>
-                                <tr>
-                                    <tb className="table_font">秋田県  <br/>
-                                    ○○市・××市・△△市
-                                    </tb>
-                                </tr>
-                            </table>
-                        </Button>
+                    <Button color='default' variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' style={{ margin: "5%"}}>
+                        <table className="place">
+                            <tr>
+                                <td><img src={akita}  height="200" alt="logo" /></td>
+                            </tr>
+                            <tr>
+                                <tb className="table_font">{areaInfoDictList.length ? areaInfoDictList[1]["middle_class_name"] : ""}<br/>
+                                {areaInfoDictList.length ? areaInfoDictList[1]["small_class_name"] : ""}
+                                </tb>
+                            </tr>
+                        </table>
+                    </Button>
 
-                        <Button color="default" variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
-                            <table className="place">
-                                <tr>
-                                    <td><img src={aomori}  height="200" alt="logo" /></td>
-                                </tr>
-                                <tr>
-                                    <tb className="table_font">青森県  <br/>
-                                    ○○市・××市・△△市
-                                    </tb>
-                                </tr>
-                            </table>
-                        </Button>
+                    <Button color="default" variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
+                        <table className="place">
+                            <tr>
+                                <td><img src={aomori}  height="200" alt="logo" /></td>
+                            </tr>
+                            <tr>
+                                <tb className="table_font">{areaInfoDictList.length ? areaInfoDictList[2]["middle_class_name"] : ""}<br/>
+                                {areaInfoDictList.length ? areaInfoDictList[2]["small_class_name"] : ""}
+                                </tb>
+                            </tr>
+                        </table>
+                    </Button>
 
-                </div>
-                
-                <h1>
-                <div className='return_to_top'>
-                    <Box textAlign='center'>
-                        <Button color="secondary" variant="contained" size="large" onClick={handleClick_select2} component={Link} to="/" endIcon={<ArrowBackIosIcon />}>
-                            トップに戻る
-                        </Button>
-                    </Box>
-                </div>
-                </h1>
             </div>
             
-
-        );
-    }
+            <h1>
+            <div className='return_to_top'>
+                <Box textAlign='center'>
+                    <Button color="secondary" variant="contained" size="large" onClick={handleClick_select2} component={Link} to="/" endIcon={<ArrowBackIosIcon />}>
+                        トップに戻る
+                    </Button>
+                </Box>
+            </div>
+            </h1>
+            <br />
+        </div>
+    );
+    
 }
  
-export default selectPage;  
+export default SelectPage;  
