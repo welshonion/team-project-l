7c7074ada641c86f39f4e9e8203c6a3bf1fc5fe7
keito-omura <78069712+keito-omura@users.noreply.github.com>
2023-01-12 13:33:33 +0900

    セレクト画面、地図背景色を変更しました

--- frontend/src/components/selectPage.js
+++ frontend/src/components/selectPage.js
@@ -52,7 +52,7 @@ const SelectPage = () => {
             </div>
             {console.log(areaInfoDictList)}
             <div className='select'>
-                    <Button color="default" variant="contained" size='large' onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
+                    <Button color="default" variant="contained" size='large' onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' backgroundColor: "#ffffff">
                         <table className="place">
                             <tr>
                                 <td><img src={aichi} width="200" alt="logo" /></td>
@@ -67,7 +67,7 @@ const SelectPage = () => {
                         
                     </Button>
 
-                    <Button color='default' variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' style={{ margin: "5%"}}>
+                    <Button color='default' variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' style={{ margin: "5%",backgroundColor: "#ffffff"}}>
                         <table className="place">
                             <tr>
                                 <td><img src={akita}  height="200" alt="logo" /></td>
@@ -80,7 +80,7 @@ const SelectPage = () => {
                         </table>
                     </Button>
 
-                    <Button color="default" variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' >
+                    <Button color="default" variant="contained" size="large" onClick={handleClick_select1} component={Link} to="/result" className='figsize_select' backgroundColor: "#ffffff">
                         <table className="place">
                             <tr>
                                 <td><img src={aomori}  height="200" alt="logo" /></td>
